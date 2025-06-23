3772e39046c66ceee74f27ce294b669785ef05b5
kanishk-singh19 <23bcs046@nith.ac.in>
2025-06-24 02:03:47 +0530

    completed hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ButtonGradient from './assets/svg/ButtonGradient';
 
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Benefits from './components/Benefits';
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
     <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
     <Header />
     <Hero />
+    <Benefits />
     </div>
-    <ButtonGradient />
+    <Button />
     </>
   );
 }
